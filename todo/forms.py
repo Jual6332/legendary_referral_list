7b339209751406940205a2b286579f1f8131d7b9
Justin Alvey <jual6332@colorado.edu>
2019-07-16 12:42:15 -0600

    Housekeeping

--- todo/forms.py
+++ todo/forms.py
@@ -1,6 +1,6 @@
-from django import forms 
+from django import forms
 
 class TodoForm(forms.Form):
-    text = forms.CharField(max_length=40, 
+    text = forms.CharField(max_length=40,
         widget=forms.TextInput(
-            attrs={'class' : 'form-control', 'placeholder' : 'Enter todo e.g. Delete junk files', 'aria-label' : 'Todo', 'aria-describedby' : 'add-btn'}))
+            attrs={'class' : 'form-control', 'placeholder' : 'Enter referral link e.g. Wolverines', 'aria-label' : 'Todo', 'aria-describedby' : 'add-btn'}))
